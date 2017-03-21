054dca0bef34e46372ffbc99276b5fcc5bf23709
jsap1904 <sapinoso.j@gmail.com>
2017-03-20 22:26:30 -0700

    date,toggle,erase

--- js/main.js
+++ js/main.js
@@ -4,43 +4,34 @@ function todoList() {
 
 	var username = document.getElementById("username").value
 	var comment = document.getElementById("comment").value
-
+	var date = new Date()
+	var dateText = document.createTextNode("date: " + date)
 	var userNameText = document.createTextNode("Username: " + username)
 	var commentText = document.createTextNode("Comment: " + comment)
-
+	
 	var newListItem = document.createElement("LI")
 	newListItem.className = "list-group-item"
 	var newUserName = document.createElement("H3")
 	newUserName.appendChild(userNameText)
+	var newDate = document.createElement("H4")
+	newDate.appendChild(dateText)
 	var newComment = document.createElement("P")
 	newComment.appendChild(commentText)
 
+	newListItem.appendChild(newDate)
 	newListItem.appendChild(newUserName)
 	newListItem.appendChild(newComment)
-	document.getElementById("commentList").appendChild(newListItem)
-	
+	document.getElementById("commentList").appendChild(newListItem).n
+	// document.getElementById("commentList").
 }
 //function that erases the comments
 
 function removeLi() {
 
-    var username = document.getElementById("username").value
-	var comment = document.getElementById("comment").value
+	var item = document.getElementById("commentList").style.visibility="hidden"
+   
+}
 
-	var userNameText = document.createTextNode("Username: " + username)
-	var commentText = document.createTextNode("Comment: " + comment)
-
-	var newListItem = document.createElement("LI")
-	newListItem.className = "list-group-item"
-	var newUserName = document.createElement("H3")
-	newUserName.appendChild(userNameText)
-	var newComment = document.createElement("P")
-	newComment.appendChild(commentText)
-
-	newListItem.appendChild(newUserName)
-	newListItem.appendChild(newComment)
-	document.getElementById("commentList").appendChild(newListItem)
-
-	var item = document.getElementById("commentList")
-    item.parentNode.removeChild(item)
+function toggleLi () {
+	var show = document.getElementById("commentList").style.visibility="initial"
 }
