798c10dc5bc0f75857c80ab7447ab08bae264eb1
jsap1904 <sapinoso.j@gmail.com>
2017-03-20 23:17:33 -0700

    finish

--- js/main.js
+++ js/main.js
@@ -5,24 +5,36 @@ function todoList() {
 	var username = document.getElementById("username").value
 	var comment = document.getElementById("comment").value
 	var date = new Date()
+	
 	var dateText = document.createTextNode("date: " + date)
 	var userNameText = document.createTextNode("Username: " + username)
 	var commentText = document.createTextNode("Comment: " + comment)
-	
+	var getPic = document.createTextNode("img/add.png")
 	var newListItem = document.createElement("LI")
 	newListItem.className = "list-group-item"
+
+	$(".button").ready(function(){
+   		$(".list-group-item:odd").css("background-color", "lightgray");
+	});
+
 	var newUserName = document.createElement("H3")
 	newUserName.appendChild(userNameText)
-	var newDate = document.createElement("H4")
+	var newDate = document.createElement("H6")
 	newDate.appendChild(dateText)
 	var newComment = document.createElement("P")
 	newComment.appendChild(commentText)
+	var pic = document.createElement("IMG")
+	pic.setAttribute("src", "img/add.png");
+	pic.setAttribute("width", "60");
+    pic.setAttribute("height", "40");
+	pic.appendChild(getPic)
 
 	newListItem.appendChild(newDate)
 	newListItem.appendChild(newUserName)
 	newListItem.appendChild(newComment)
+	newListItem.appendChild(pic)
 	document.getElementById("commentList").appendChild(newListItem).n
-	// document.getElementById("commentList").
+
 }
 //function that erases the comments
 
@@ -35,3 +47,4 @@ function removeLi() {
 function toggleLi () {
 	var show = document.getElementById("commentList").style.visibility="initial"
 }
+
